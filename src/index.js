3d4631eb4e12dcf3d37f52c0843ddd51dea3be96
plasticine <975036719@qq.com>
2022-09-23 20:29:09 +0800

    feat: a simple ssr-server demo

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const childProcess = require('child_process')
+
+const app = express()
+
+app.get('*', (req, resp) => {
+  resp.send(`
+    <html>
+      <body>
+        <div>hello-ssr</div>
+      </body>
+    </html>
+  `)
+})
+
+app.listen(3000, () => {
+  console.log('ssr-server listen on 3000')
+})
+
+childProcess.exec('start http://localhost:3000')
