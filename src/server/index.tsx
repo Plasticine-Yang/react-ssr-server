bbd8433dd6d9f5bc057f847f91f084ec0dbbbd38
plasticine <975036719@qq.com>
2022-09-25 10:21:27 +0800

    feat: 加入客户端代码，完成事件的绑定

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,13 +1,22 @@
-import express from 'express'
+import Home from '@/pages/Home'
 import childProcess from 'child_process'
+import express from 'express'
+import path from 'path'
+import { renderToString } from 'react-dom/server'
 
 const app = express()
+const content = renderToString(<Home />)
+
+// 将项目根目录下的 client_build 作为静态资源服务器的根目录
+// 这样在注入客户端脚本的时候就能通过 src="/index.js" 访问到客户端打包结果
+app.use(express.static(path.resolve(process.cwd(), 'client_build')))
 
 app.get('*', (req, resp) => {
   resp.send(`
     <html>
       <body>
-        <div>hello-ssr</div>
+        <div id="root">${content}</div>
+        <script src="/index.js"></script>
       </body>
     </html>
   `)
